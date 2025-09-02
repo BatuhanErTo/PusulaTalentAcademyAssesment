860834884312045262536e55e76fea1dcf1e1cd2
Batuhan Erol <bataer57@gmail.com>
2025-09-02 11:37:30 +0300

    Update the name of classes to comply with the given format of assesment

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿//List<int> numbers = new List<int> {1,2,2,3};
 //var result = Question1.MaxIncreasingSubArrayAsJson(numbers);
 //Console.WriteLine(result);
-List<string> words = new List<string> {"miscellaneous", "queue", "sky", "cooperative", "MEEAİÜhaberüĞ"};
-var result = Question2.LongestVowelSubsequenceAsJson(words);
-Console.WriteLine(result);
+//List<string> words = new List<string> {"miscellaneous", "queue", "sky", "cooperative", "MEEAİÜhaberüĞ"};
+//var result = Question2.LongestVowelSubsequenceAsJson(words);
+//Console.WriteLine(result);
 
 /*
 QUESTION-1
